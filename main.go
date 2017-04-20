5aa6e03530c71420ce83fd3e460185c364309574
Louis Taylor <louis@kragniz.eu>
2017-04-20 15:17:56 +0100

    Remove a lot of code

--- main.go
+++ main.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"log"
 	"time"
 
 	"github.com/coreos/etcd/clientv3"
 )
 
 func main() {
-	cli, err := clientv3.New(clientv3.Config{
+	cli, _ := clientv3.New(clientv3.Config{
 		Endpoints:   []string{"localhost:2379"},
 		DialTimeout: 5 * time.Second,
 	})
 	defer cli.Close()
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	resp, err := cli.Get(ctx, "/something")
-	cancel()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, ev := range resp.Kvs {
-		fmt.Println(ev.Value)
-	}
 }
